780d7087bcfc39a2b1b4d78f7dd32f2f21e59a34
Yohandel Cuevas <ycuevas@isf.do>
2022-05-30 09:11:29 -0400

    setting up project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { IncomesComponent } from './pages/incomes/incomes.component';
+import { ExpensesComponent } from './pages/expenses/expenses.component';
+import { AddModalComponent } from './components/add-modal/add-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    IncomesComponent,
+    ExpensesComponent,
+    AddModalComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
